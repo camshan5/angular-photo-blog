4d230422e16764a5cf98f6ea98f924ed4949e664
Cameron Shannon <cameroncshannon@gmail.com>
2020-02-12 21:46:43 -0800

    Completed project set up

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+// Root module to the application
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ wwwroot/app.7075be9d9ef8de1c3819.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],[function(t,e,c){"use strict";c(1)},function(t,e){t.exports="// removed by extract-text-webpack-plugin"}]);
+//# sourceMappingURL=app.7075be9d9ef8de1c3819.js.map
