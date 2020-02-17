06f830fefd6105df7202c5b2449140aefa16401d
Cameron Shannon <cameroncshannon@gmail.com>
2020-02-17 10:08:08 -0800

    S3V4 - Connecting a Service and Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
 import { AppComponent } from "./app.component";
 import { EntryListComponent, EntryComponent, EntryService } from "./entries";
+import { InMemoryEntryService } from "./backend";
 
 @NgModule({
-  imports: [BrowserModule, HttpModule],
+  // only use imports for Angular modules
+  imports: [
+    BrowserModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryEntryService)
+  ],
   providers: [EntryService],
   declarations: [AppComponent, EntryComponent, EntryListComponent],
   bootstrap: [AppComponent]
--- src/app/entries/entry-list/entry-list.component.ts
+++ src/app/entries/entry-list/entry-list.component.ts
@@ -1,12 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { EntryService } from "..";
+import { Entry } from "../shared/entry.model";
 
 @Component({
   selector: "app-entry-list",
   templateUrl: "entry-list.component.html",
   styleUrls: ["entry-list.component.css"]
 })
-export class EntryListComponent {
-  constructor() {
+export class EntryListComponent implements OnInit {
+  entries: Entry[]; // create entries property
 
+  constructor(private entryService: EntryService) {}
+
+  ngOnInit() {
+    // set components entries prop to be the value returned
+    this.entryService.getEntries().then(entries => (this.entries = entries));
   }
 }
