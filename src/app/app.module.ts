b5ee8c99b8eaac4d5aa9544e9a8fe20b64174f3a
Cameron Shannon <cameroncshannon@gmail.com>
2020-02-18 10:47:03 -0800

    Added form components and requirements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
+
 import { InMemoryWebApiModule } from "angular-in-memory-web-api";
 import { AppComponent } from "./app.component";
-import { EntryListComponent, EntryComponent, EntryService } from "./entries";
+import {
+  EntryListComponent,
+  EntryComponent,
+  EntryService,
+  EntryCommentFormComponent
+} from "./entries";
 import { InMemoryEntryService } from "./backend";
 
 @NgModule({
@@ -11,10 +18,16 @@ import { InMemoryEntryService } from "./backend";
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     InMemoryWebApiModule.forRoot(InMemoryEntryService)
   ],
   providers: [EntryService],
-  declarations: [AppComponent, EntryComponent, EntryListComponent],
+  declarations: [
+    AppComponent,
+    EntryComponent,
+    EntryListComponent,
+    EntryCommentFormComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-entry-comment-form",
+  templateUrl: "entry-comment-form.component.html"
+})
+export class EntryCommentFormComponent {}
