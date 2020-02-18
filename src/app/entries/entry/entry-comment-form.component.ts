9c2bdb9fac9f081829200116e9129f1a20549fa3
Cameron Shannon <cameroncshannon@gmail.com>
2020-02-18 11:47:19 -0800

    Added support to reset form after submitting data

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Output, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-entry-comment-form",
@@ -10,13 +11,19 @@ export class EntryCommentFormComponent {
   comment: string = "";
 
   // add prop to set up EventEmitter (as a <generic>)
-  @Output() onCommentAdded = new EventEmitter<{ name: string; comment: string }>();
+  @Output() onCommentAdded = new EventEmitter<{
+    name: string;
+    comment: string;
+  }>();
 
+  // ViewChild binds local variables found in the form
+  @ViewChild("commentForm") commentForm: NgForm;
   // add onsubmit method
-  onSubmit() {
+  onSubmit(commentForm: NgForm) {
     // use debugger statement to test submitEvent binding
     // debugger;
     let comment = { name: this.name, comment: this.comment }; // create the comment
     this.onCommentAdded.emit(comment); // emit the comment
+    this.commentForm.resetForm(); // clear the form after submitting
   }
 }
