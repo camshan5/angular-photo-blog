be8cd8857fb61e4f53dc61c890026703edf2be3a
Cameron Shannon <cameroncshannon@gmail.com>
2020-02-19 13:13:19 -0800

    Completed Section5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
 import { FormsModule } from "@angular/forms";
-
 import { InMemoryWebApiModule } from "angular-in-memory-web-api";
 import { AppComponent } from "./app.component";
 import {
--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,9 +1,9 @@
 import {
   Component,
   EventEmitter,
+  Input,
   Output,
-  ViewChild,
-  Input
+  ViewChild
 } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { EntryService } from "..";
@@ -16,28 +16,25 @@ export class EntryCommentFormComponent {
   // add a name and comment property
   name: string = "";
   comment: string = "";
-
   @Input() entryId: number;
   // add prop to set up EventEmitter (as a <generic>)
   @Output() onCommentAdded = new EventEmitter<{
     name: string;
     comment: string;
   }>();
-
   // ViewChild binds local variables found in the form,
   // lets the component inspect the template for local variables
   // and assign them to members of a component.
   @ViewChild("commentForm") commentForm: NgForm;
-
   // specify private to tell TypeScript to append the to the prototype
   // making it available on the instance of the class
   constructor(private entryService: EntryService) {}
 
   onSubmit(commentForm: NgForm) {
-
+    // Check if form is valid
+    if (this.commentForm.invalid) return;
     // create the comment
     let comment = { name: this.name, comment: this.comment };
-
     this.entryService.addComment(this.entryId, comment).then(() => {
       // emit the comment
       this.onCommentAdded.emit(comment);
@@ -45,5 +42,4 @@ export class EntryCommentFormComponent {
       this.commentForm.resetForm();
     });
   }
-
 }
